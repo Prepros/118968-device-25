a89d8dde6aa6499c526439b461f50ea4722786ab
Sergey Erofeev <erofeev28_9@mail.ru>
2019-06-29 14:01:20 +0600

    Исправил ошибки критериев

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ try {
 }
 
 // Открываем модальное окно с формой обратной связи
-aboutFeedback.addEventListener("click", function(evt) {
+aboutFeedback.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (!modalContact.classList.contains("open") && !overlay.classList.contains("open")) {
     overlay.classList.add("open");
@@ -51,30 +51,31 @@ aboutFeedback.addEventListener("click", function(evt) {
 formFeedback.addEventListener("submit", function (evt) {
   if (!inputName.value) {
     evt.preventDefault();
-    if (!inputName.classList.contains("input-error")) {
-      inputName.classList.add("input-error");
-      inputName.placeholder = "Это поле обязательно для заполнения";
-    }
+    inputName.classList.remove("input-error");
+    inputName.offsetWidth = inputName.offsetWidth;
+    inputName.classList.add("input-error");
+    inputName.placeholder = "Это поле обязательно для заполнения";
   } else {
+    inputName.classList.remove("input-error");
     localStorage.setItem("name", inputName.value);
   }
 
   if (!inputEmail.value) {
     evt.preventDefault();
-    if (!inputEmail.classList.contains("input-error")) {
-      inputEmail.classList.add("input-error");
-      inputEmail.placeholder = "Это поле обязательно для заполнения";
-    }
+    inputEmail.classList.remove("input-error");
+    inputEmail.offsetWidth = inputEmail.offsetWidth;
+    inputEmail.classList.add("input-error");
+    inputEmail.placeholder = "Это поле обязательно для заполнения";
   } else {
     localStorage.setItem("email", inputEmail.value);
   }
 
   if (!inputText.value) {
     evt.preventDefault();
-    if (!inputText.classList.contains("input-error")) {
-      inputText.classList.add("input-error");
-      inputText.placeholder = "Это поле обязательно для заполнения";
-    }
+    inputText.classList.remove("input-error");
+    inputText.offsetWidth = inputText.offsetWidth;
+    inputText.classList.add("input-error");
+    inputText.placeholder = "Это поле обязательно для заполнения";
   }
 
   if (!inputName.value && !inputEmail.value && !inputText.value) {
@@ -87,7 +88,7 @@ formFeedback.addEventListener("submit", function (evt) {
 });
 
 // Открываем модальное окно с картой проезда
-aboutMap.addEventListener("click", function(evt) {
+aboutMap.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (!modalMap.classList.contains("open") && !overlay.classList.contains("open")) {
     overlay.classList.add("open");
@@ -97,7 +98,7 @@ aboutMap.addEventListener("click", function(evt) {
 
 // Закрытие модальных окон через кнопку Закрыть
 for (i = 0; i < modalClose.length; i++) {
-  modalClose[i].addEventListener("click", function(evt) {
+  modalClose[i].addEventListener("click", function (evt) {
     evt.preventDefault();
     for (j = 0; j < modal.length; j++) {
       if (modal[j].classList.contains("open") && overlay.classList.contains("open")) {
@@ -109,7 +110,7 @@ for (i = 0; i < modalClose.length; i++) {
 }
 
 // Закрытие модальных окон через клавишу Esc
-window.addEventListener("keydown", function(evt) {
+window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     for (i = 0; i < modal.length; i++) {
@@ -122,30 +123,30 @@ window.addEventListener("keydown", function(evt) {
 });
 
 ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-        center: [55.68697956906804,37.52965449999998],
-        zoom: 16,
-        controls: ['zoomControl']
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+  var myMap = new ymaps.Map('map', {
+      center: [55.68697956906804, 37.52965449999998],
+      zoom: 16,
+      controls: ['zoomControl']
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
 
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Мы здесь',
-            balloonContent: 'Интерет магазин Device'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: '../img/yandex.png',
-            // Размеры метки.
-            iconImageSize: [40, 60],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        });
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Мы здесь',
+      balloonContent: 'Интерет магазин Device'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: '../img/yandex.png',
+      // Размеры метки.
+      iconImageSize: [40, 60],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+    });
 
-    myMap.geoObjects
-        .add(myPlacemark);
+  myMap.geoObjects
+    .add(myPlacemark);
 });
